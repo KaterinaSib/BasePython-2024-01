384bd3cc2e5284d5a5b847840d08d9098329a439
Katerina <katerina--777@mail.ru>
2024-08-02 02:58:02 +0700

    fix settings and add .env

--- water_metering/settings.py
+++ water_metering/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = "water_metering.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "meter",
-        "USER": "user",
-        "PASSWORD": "password",
-        "HOST": "",
-        "PORT": "5432",
+        "NAME": os.getenv('POSTGRES_DB'),
+        "USER": os.getenv('POSTGRES_USER'),
+        "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
+        "HOST": os.getenv('POSTGRES_HOST'),
+        "PORT": os.getenv('POSTGRES_PORT'),
     }
 }
 
