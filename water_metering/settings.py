5cf6bc061f741adf95b40b805a5e2aa8c0c90de2
Katerina <katerina--777@mail.ru>
2024-08-04 23:42:43 +0700

    change script in gitlab-ci

--- water_metering/settings.py
+++ water_metering/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "NAME": os.getenv('POSTGRES_DB', 'meter'),
         "USER": os.getenv('POSTGRES_USER', 'user'),
         "PASSWORD": os.getenv('POSTGRES_PASSWORD', "password"),
-        "HOST": os.getenv('POSTGRES_HOST', "pg"),
+        "HOST": os.getenv('POSTGRES_HOST', "localhost"),
         "PORT": 5432,
     }
 }
